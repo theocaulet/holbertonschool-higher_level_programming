825f2f162466b15e400a766d68069891a7be5ca3
theocaulet <caulet.theo@gmail.com>
2026-01-13 13:04:35 +0100

    fix: Correct print statement and formatting in 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for first_digit in range(0, 10):
     for second_digit in range(0, 10):
         if first_digit == 8 and second_digit == 9:
-            print("{:d}{:d}".format(first_digit, second_digit))
-        if first_digit < second_digit:
+            print("{:d}{:d}".format(first_digit, second_digit), end='\n')
+        elif first_digit < second_digit:
             print("{:d}{:d}".format(first_digit, second_digit), end=', ')
