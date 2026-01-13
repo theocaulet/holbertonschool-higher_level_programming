078d46c0451224723c779ec19906b04b5da99f92
theocaulet <caulet.theo@gmail.com>
2026-01-13 14:49:20 +0100

    fix: Refactor uppercase function to accumulate uppercase characters before printing

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    upper = ""
     for char in str:
-        if 97 <= ord(char) <= 122:
-            print("{}".format(chr(ord(char) - 32)), end="")
+        if 97 <= char <= 122:
+            upper += chr(ord(char) - 32)
         else:
-            print("{}".format(char), end="")
-    print()
+            upper += char
+    print("{}".format(upper))
