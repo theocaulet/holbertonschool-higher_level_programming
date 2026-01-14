f684917ed63a44d3703f4fd62bf68aa5cd115ffa
theocaulet <caulet.theo@gmail.com>
2026-01-14 12:32:52 +0100

    fix: Update output message for single argument case in 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     if argc == 0:
         print("0 arguments.")
     elif argc == 1:
-        print("1 Hello:")
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments".format(argc))
         for i in range(1, argc + 1):
