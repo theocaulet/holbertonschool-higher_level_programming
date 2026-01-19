36f7aeea4b7615c288bef1ca62e901728fc5c722
theocaulet <caulet.theo@gmail.com>
2026-01-19 15:55:07 +0100

    feat: Implement max_integer function to find the maximum value in a list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_value = my_list[0]
+    for M in my_list:
+        if M > max_value:
+            max_value = M
+    if my_list == "":
+        return None
+    else:
+        return (max_value)
