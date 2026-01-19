811210f0e1ad247ce0f2472151630165bb4cb2a4
theocaulet <caulet.theo@gmail.com>
2026-01-19 15:20:52 +0100

    feat: Refactor add_tuple function to handle tuple length cases more effectively

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     t = tuple_a + tuple_b
-    t += (0, 0)
+    if len(t) < 2:
+        t += (0, 0, 0, 0)
+    elif len(t) > 2:
+        t = t[:4]
+    t += (0, 0, 0, 0)
     return (t[0] + t[2], t[1] + t[3])
