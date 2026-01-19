b03adc529b871ae48f069452c4dc79ab24757b29
theocaulet <caulet.theo@gmail.com>
2026-01-19 15:36:39 +0100

    feat: Refactor add_tuple function for improved tuple handling and clarity

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    t = tuple_a + tuple_b
-    if len(t) < 2:
-        t += (0, 0, 0, 0)
-    elif len(t) > 2:
-        t = t[:4]
-    t += (0, 0, 0, 0)
-    return (t[0] + t[2], t[1] + t[3])
+    a = [0, 0]
+    b = [0, 0]
+    if len(tuple_a) > 0:
+        a[0] = tuple_a[0]
+    else:
+        a[0] = 0
+    if len(tuple_a) > 1:
+        a[1] = tuple_a[1]
+    else:
+        a[1] = 0
+    if len(tuple_b) > 0:
+        b[0] = tuple_b[0]
+    else:
+        b[0] = 0
+    if len(tuple_b) > 1:
+        b[1] = tuple_b[1]
+    else:
+        b[1] = 0
+    return (a[0] + b[0], a[1] + b[1])
