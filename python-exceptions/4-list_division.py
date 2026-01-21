cbc7e94842af73490a0a11b8ec91bb8b9f47159b
theocaulet <caulet.theo@gmail.com>
2026-01-21 15:32:49 +0100

    fix: Simplify variable usage in list_division function by removing temp_result

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    temp_result = 0
     result = []
-    div = 0
-    for i in range(0, list_length):
+    for i in range(list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
-            temp_result = div
         except ZeroDivisionError:
-            temp_result = 0 
+            div = 0 
             print("division by 0")
         except IndexError:
-            temp_result = 0
+            div = 0
             print("out of range")
         except TypeError:
-            temp_result = 0
+            div = 0
             print("wrong type")
         finally:
-            result.append(temp_result)
-            temp_result = 0
+            result.append(div)
+            div = 0
     return result
