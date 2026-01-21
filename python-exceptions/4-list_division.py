7087ca92bcaecace2947d2b83afccf14d345559f
theocaulet <caulet.theo@gmail.com>
2026-01-21 14:51:51 +0100

    fix: Improve type checking in list_division function to ensure correct error messages

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -10,15 +10,13 @@ def list_division(my_list_1, my_list_2, list_length):
             result.append(0)
             print("division by 0")
         except TypeError:
-            if isinstance(my_list_1[i], (int, float)):
+            if not isinstance(my_list_1[i], (int, float)):
                 print("wrong type")
                 result.append(0)
-            elif isinstance(my_list_2[i], (int, float)):
+            elif not isinstance(my_list_2[i], (int, float)):
                 print("wrong type")
                 result.append(0)
         except IndexError:
             print("out of range")
             result.append(0)
-        finally:
-            result.append(result)
     return result
