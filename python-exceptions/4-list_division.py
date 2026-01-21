d27500d04e5071cc70706096ed7174590ac662bb
theocaulet <caulet.theo@gmail.com>
2026-01-21 15:41:37 +0100

    fix: Remove redundant assignment of div in list_division function

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,12 +8,11 @@ def list_division(my_list_1, my_list_2, list_length):
             div = 0
             print("wrong type")
         except ZeroDivisionError:
-            div = 0 
+            div = 0
             print("division by 0")
         except IndexError:
             div = 0
             print("out of range")
         finally:
             result.append(div)
-            div = 0
     return result
