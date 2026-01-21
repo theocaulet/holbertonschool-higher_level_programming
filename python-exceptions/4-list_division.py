00574e3ed9b70ef5947eb30bb0e90779209541aa
theocaulet <caulet.theo@gmail.com>
2026-01-21 15:01:02 +0100

    fix: Refactor list_division function to improve result handling and error reporting

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    temp_result = 0
     result = []
     div = 0
     for i in range(0, list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
-            result.append(div)
+            temp_result = div
         except ZeroDivisionError:
-            result.append(0)
+            temp_result = 0 
             print("division by 0")
         except TypeError:
             if not isinstance(my_list_1[i], (int, float)):
                 print("wrong type")
-                result.append(0)
+                temp_result = 0
             elif not isinstance(my_list_2[i], (int, float)):
                 print("wrong type")
-                result.append(0)
+                temp_result = 0
         except IndexError:
             print("out of range")
-            result.append(0)
+            temp_result = 0
+        finally:
+            result.append(temp_result)
+            temp_result = 0
     return result
