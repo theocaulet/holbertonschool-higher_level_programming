d83f898d0150dac14d1d8248209afecac9462ddb
theocaulet <caulet.theo@gmail.com>
2026-01-21 13:26:58 +0100

    fix: Refactor safe_print_division to handle division result and return value correctly

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None
     try:
         result = a / b
     except ZeroDivisionError:
-        return None
-    else:
-        return result
+        result = None
     finally:
         if b == 0:
             print("Inside result: None")
         else:
             print("Inside result: {}".format(result))
+    return result
