18e19b5547b5d0cd61ce0c900b0bfa71977bde91
theocaulet <caulet.theo@gmail.com>
2026-01-21 15:35:27 +0100

    fix: Reorder TypeError handling in list_division function for clarity

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,15 +4,15 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            div = 0
+            print("wrong type")
         except ZeroDivisionError:
             div = 0 
             print("division by 0")
         except IndexError:
             div = 0
             print("out of range")
-        except TypeError:
-            div = 0
-            print("wrong type")
         finally:
             result.append(div)
             div = 0
