7268c436e0a4b515b0cb19c10903d0facc27a6fe
theocaulet <caulet.theo@gmail.com>
2026-01-22 12:28:30 +0100

    fix: Simplify type checks for add_integer function to enhance readability

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -18,8 +18,8 @@ def add_integer(a, b=98):
     Raises:
     TypeError: if a and b are not integers or floats
     """
-    if isinstance(a, (int, float)) is False:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if isinstance(b, (int, float)) is False:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
