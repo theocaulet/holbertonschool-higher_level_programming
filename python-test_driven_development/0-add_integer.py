68a9bde1aca0649b7ba7b248adc3c2d1ec783c95
theocaulet <caulet.theo@gmail.com>
2026-01-22 10:44:03 +0100

    feat: Implement add_integer function with type validation and error handling

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    Add two integers and return the result.
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Return:
+    The sum of a and b
+
+    Raises:
+    TypeError: if a and b are not integers or floats
+    """
+    if isinstance(a, (int, float)) is False:
+        raise TypeError("a must be an integer")
+    if isinstance(b, (int, float)) is False:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
