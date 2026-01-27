a447d48ee02c40b8853b812c98c3bb6ebebdee15
theocaulet <caulet.theo@gmail.com>
2026-01-27 10:50:15 +0100

    refactor: Reorganize property and setter methods in Rectangle class

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -12,33 +12,33 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-        @property
-        def width(self):
-            """Retrieve the width of the rectangle."""
-            return self.__width
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """Set the width of the rectangle."""
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """Retrieve the height of the rectangle."""
-            return self.__height
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle."""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """Set the height of the rectangle."""
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """Define the area of the rectangle."""
