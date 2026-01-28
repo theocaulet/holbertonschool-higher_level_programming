f2622a57590852617fe5e4861badf971a8cf8477
theocaulet <caulet.theo@gmail.com>
2026-01-28 10:08:16 +0100

    fix: Use property setters in Rectangle constructor for width and height

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -3,6 +3,7 @@
 
 
 class Rectangle:
+    """Represent an empty Rectangle."""
     number_of_instances = 0
     print_symbol = "#"
 
@@ -12,8 +13,8 @@ class Rectangle:
         width: The width of the rectangle
         height: The height of the rectangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
