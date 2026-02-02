1bcc4d5646ec6ea174e11c61e8cb013c0866a95b
theocaulet <caulet.theo@gmail.com>
2026-02-02 10:28:46 +0100

    fix: Update docstrings for MyList class and print_sorted method

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 class MyList(list):
+    """Create a class Mylist that inherits from list."""
     def print_sorted(self):
+        """Print the list, but sorted."""
         result = sorted(self)
         print(MyList(result))
