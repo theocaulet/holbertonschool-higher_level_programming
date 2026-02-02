e98697de97b5fe632990814cfe826c6753c71f02
theocaulet <caulet.theo@gmail.com>
2026-02-02 13:15:51 +0100

    feat: Implement BaseGeometry class with area method raising exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Create a class BaseGeometry"""
+    def area(self):
+        """Public instance method that raises an exception."""
+        raise Exception("area() is not implemented")
