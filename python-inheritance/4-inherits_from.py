2910ff0eec040dea95cb48cc6bf124498b010aa0
theocaulet <caulet.theo@gmail.com>
2026-02-02 10:57:23 +0100

    fix: Update docstring for inherits_from function to clarify its purpose

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
+    """Function that returns True if the object is an instance,
+      otherwise False."""
     if isinstance(obj, a_class):
         return True
     else:
