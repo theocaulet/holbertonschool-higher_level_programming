cb7b68ff7b34b5f010412476499957565c065f97
theocaulet <caulet.theo@gmail.com>
2026-02-02 15:23:43 +0100

    feat: Add __str__ method to Rectangle class for string representation

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -25,3 +25,6 @@ class Rectangle(BaseGeometry):
     def area(self):
         """Return the area of the rectangle."""
         return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
