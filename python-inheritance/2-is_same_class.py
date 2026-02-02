9870e9d218cbef4062512eb97903d5e1b669098d
theocaulet <caulet.theo@gmail.com>
2026-02-02 11:36:34 +0100

    fix: Refine is_same_class function logic to ensure accurate class type checking

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,7 +2,7 @@
 def is_same_class(obj, a_class):
     """Function that returns True if the object is exactly
       an instance of the specified class; otherwise False."""
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) is a_class:
         return True
     else:
         return False
