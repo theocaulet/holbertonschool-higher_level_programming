441697112e12ba73b04859a79ae3766553a799bb
theocaulet <caulet.theo@gmail.com>
2026-02-02 13:36:53 +0100

    feat: Add BaseGeometry class with integer validation method

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Create a class BaseGeometry"""
+    def area(self):
+        """Public instance method that raises an exception."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value."""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
