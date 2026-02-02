183d752b9fb7d5fdda03ed22050f985adf24f45d
theocaulet <caulet.theo@gmail.com>
2026-02-02 15:52:04 +0100

    feat: Implement Animal, Dog, and Cat classes with sound methods

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+"""Import the necessary components from the abc module."""
+
+
+class Animal(ABC):
+    """Create an Animal class."""
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    """Create a Dog class."""
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    """Create a Cat class."""
+    def sound(self):
+        return "Meow"
