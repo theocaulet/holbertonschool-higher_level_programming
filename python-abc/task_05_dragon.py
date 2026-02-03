f53c309c95a18d6a1a10a307fd2ff9212fab3b09
theocaulet <caulet.theo@gmail.com>
2026-02-03 15:26:14 +0100

    feat: Add SwimMixin and FlyMixin classes with Dragon class implementation

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class SwimMixin:
+    """Create a mixin class for swimming behavior."""
+    def swim(self):
+        """Define swim method."""
+        print("The creature swims!")
+
+
+class FlyMixin:
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        print("The dragon roars!")
