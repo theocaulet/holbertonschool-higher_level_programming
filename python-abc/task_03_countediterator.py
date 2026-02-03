4cace64910e1e85913d9ae40000a5eaf8a197628
theocaulet <caulet.theo@gmail.com>
2026-02-03 14:19:03 +0100

    feat: Add docstrings to CountedIterator class methods for better clarity

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 class CountedIterator:
+    """Create a class named CountedIterator."""
     def __init__(self, iterable):
+        """Initialize the CountedIterator with an iterable."""
         self.iterable = iter(iterable)
         self.count = 0
 
     def __iter__(self):
+        """Define the iterator method."""
         return self
 
     def get_count(self):
+        """Define a method to get the count of iterations."""
         return self.count
 
     def __next__(self):
+        """Define the next method to return the next item."""
         self.count += 1
         return next(self.iterable)
