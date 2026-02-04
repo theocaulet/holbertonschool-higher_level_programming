3daadc600e7d10fc043444088f1c09dab7fb3cfa
theocaulet <caulet.theo@gmail.com>
2026-02-04 09:41:34 +0100

    feat: Add module docstring to 3-is_kind_of_class.py for better clarity

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module that defines a function to check if an object is an instance
+ of a class or its subclass."""
+
+
 def is_kind_of_class(obj, a_class):
     """Function that returns True if the object is an instance of, or if the
       object is an instance of a class that inherided from the specified class;
