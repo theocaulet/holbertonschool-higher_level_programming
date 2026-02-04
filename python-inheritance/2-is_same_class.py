560284a9271f0dcc2cbab35e11bd165a33848258
theocaulet <caulet.theo@gmail.com>
2026-02-04 09:39:33 +0100

    feat: Add module docstring to 2-is_same_class.py for better clarity

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that defines a function to check class of an object."""
+
+
 def is_same_class(obj, a_class):
     """Function that returns True if the object is exactly
       an instance of the specified class; otherwise False."""
