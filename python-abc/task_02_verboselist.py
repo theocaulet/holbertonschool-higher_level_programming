abeb6d4e5a65b1d1af318bd8701bf0452b95edd3
theocaulet <caulet.theo@gmail.com>
2026-02-04 11:14:15 +0100

    fix: Update print statements in VerboseList methods for consistent formatting

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -6,20 +6,21 @@ class VerboseList(list):
     """Create a VerboseList class."""
     def append(self, item):
         """Define the append method."""
-        print(f"Added [{item}] to the list")
+        print(f"Added {[item]} to the list")
         super().append(item)
 
     def extend(self, x):
         """Define the extend method."""
-        print(f"Extended the list with [{len(x)}] items")
+        print(f"Extended the list with {[len(x)]} items")
         super().extend(x)
 
     def remove(self, item):
         """Define the remove method."""
-        print(f"Removed [{item}] from the list")
+        print(f"Removed {[item]} from the list")
         super().remove(item)
 
     def pop(self, item=-1):
         """Define the pop method."""
-        print(f"Popped [{item}] from the list")
-        super().pop(item)
+        value = self[item]
+        print(f"Popped {[value]} from the list")
+        return super().pop(item)
