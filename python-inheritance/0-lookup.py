1d143b08ef58c14d46048695fd2f1522a841e702
theocaulet <caulet.theo@gmail.com>
2026-02-04 09:35:39 +0100

    feat: Add module docstring to clarify the purpose of the lookup function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module that defines a function to lookup
+ attributes and methods of an object."""
+
+
 def lookup(obj):
     """Return the list of attributes and methods of an object."""
     return list(dir(obj))
