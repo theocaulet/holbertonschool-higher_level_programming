372cc98f691e96e6e343c2cc416ab9d5297c0604
theocaulet <caulet.theo@gmail.com>
2026-02-09 14:24:55 +0100

    feat: Add Student class with initialization and JSON representation methods

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define a Student class."""
+
+
+class Student:
+    """Represent a student."""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+        return new_dict
