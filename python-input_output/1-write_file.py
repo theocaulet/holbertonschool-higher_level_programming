ee1e9eb789cdcf11fadd7e66c8beb608cc59273e
theocaulet <caulet.theo@gmail.com>
2026-02-09 10:33:57 +0100

    fix: Update docstring in write_file.py for clarity and consistency

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Function that writes a string to a text file and
+ returns the number of characters"""
+
+
 def write_file(filename="", text=""):
+    """Define a function that writes a string to a text file."""
     with open(filename, 'w', encoding="utf-8") as file:
         return file.write(text)
