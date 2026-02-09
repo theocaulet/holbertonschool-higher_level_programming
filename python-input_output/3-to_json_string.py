19578426435856b8f1219e5519983d89cf6c739f
theocaulet <caulet.theo@gmail.com>
2026-02-09 11:18:15 +0100

    refactor: Move import json to the top of the file for consistency

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import json
 """Function that returns the JSON representation of an object(string)."""
 
 
 def to_json_string(my_obj):
     """Define to_json_string function."""
-    import json
     return json.dumps(my_obj)
--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+import json
 """Function that writes an object to a text file,
  using a JSON representation"""
 
 
 def save_to_json_file(my_obj, filename):
     """Define a save_to_json_file function."""
-    import json
     with open(filename, 'w') as file:
         json.dump(my_obj, file)
