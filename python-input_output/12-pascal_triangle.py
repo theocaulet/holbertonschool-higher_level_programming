49720dbf5033a188d32877d9baa46c20f497f6b6
theocaulet <caulet.theo@gmail.com>
2026-02-11 09:33:03 +0100

    Fix: Remove isinstance of the function

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,8 +4,6 @@
 
 def pascal_triangle(n):
     """Returns a list of lists of integers representing Pascal's triangle"""
-    if not isinstance(n, int):
-        raise TypeError("n must be an integer")
     if n <= 0:
         return []
     triangle = []
