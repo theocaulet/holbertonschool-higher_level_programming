2c228fface946f1956bc05e1fd3fe1eee84ad406
theocaulet <caulet.theo@gmail.com>
2026-02-11 09:41:01 +0100

    fix: replace pickle with json

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Function that serializes an object to a file
  and deserializes an object from a file."""
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     """Define a serialize_and_save_to_file function."""
-    with open(filename, 'wb') as file:
-        pickle.dump(data, file)
+    with open(filename, 'w') as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """Define a load_and_deserialize function."""
-    with open(filename, 'rb') as file:
-        return pickle.load(file)
+    with open(filename, 'r') as file:
+        return json.load(file)
