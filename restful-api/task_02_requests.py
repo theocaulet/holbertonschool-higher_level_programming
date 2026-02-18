fcf63056c72cdb6dfb1c06f2fe3c59fa3db6848e
theocaulet <caulet.theo@gmail.com>
2026-02-18 13:20:45 +0100

    feat: Implement functions to fetch and print/save posts from JSONPlaceholder API

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    r = requests.get(url)
+    print(r.status_code)
+    if r.status_code == 200:
+        posts = r.json()
+        for post in posts:
+            print(post.get("title"))
+
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    r = requests.get(url)
+    if r.status_code == 200:
+        post = r.json()
+        with open("posts.csv", "w", newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(["id", "title", "body"])
+            for p in post:
+                writer.writerow([p.get("id"), p.get("title"), p.get("body")])
