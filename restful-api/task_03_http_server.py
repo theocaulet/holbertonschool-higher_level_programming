1368496cc1eb05e4e533da825397b7966f79ba58
theocaulet <caulet.theo@gmail.com>
2026-02-18 15:43:29 +0100

    fix: Remove unnecessary docstring comments and improve code clarity in HTTP server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 import http.server
 import json
-"""
-This module implements a simple HTTP server that responds to GET requests
- on two endpoints: /data and /status.
- The server listens on localhost at port 8000.
--The /data endpoint returns a JSON object containing sample data.
--The /status endpoint returns a plain text response with the message "OK".
--For any other endpoint, the server responds with a 404 Not Found error.
-"""
 
 
 PORT = 8000
@@ -16,12 +8,7 @@ HOST = "localhost"
 
 
 class HTTPserver(http.server.BaseHTTPRequestHandler):
-    """This class handles HTTP GET requests and responds with appropriate data
-      based on the requested endpoint."""
     def do_GET(self):
-        """Define the behavior for GET requests. It checks the requested path
-          and responds with JSON data for /data, a plain text message
-            for /status, and a 404 error for any other path."""
         if self.path == "/data":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
