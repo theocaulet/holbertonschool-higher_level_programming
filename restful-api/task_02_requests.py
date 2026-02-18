81e1f5531e380db1253f3d491f806446fe287261
theocaulet <caulet.theo@gmail.com>
2026-02-18 13:32:53 +0100

    fix: Improve status code output format in fetch_and_print_posts function

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,7 +6,7 @@ import csv
 def fetch_and_print_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     r = requests.get(url)
-    print(r.status_code)
+    print(f"Status Code: {r.status_code}")
     if r.status_code == 200:
         posts = r.json()
         for post in posts:
