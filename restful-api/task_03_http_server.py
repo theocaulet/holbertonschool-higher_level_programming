a769eff35b6156df6f3fd293d80fd6b5714c5eb4
theocaulet <caulet.theo@gmail.com>
2026-02-18 18:05:48 +0100

    fix: Standardize UTF-8 encoding in JSON responses and add missing response for /status endpoint

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -25,12 +25,14 @@ class HTTPserver(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
+
         elif self.path == "/data":
             data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(json.dumps(data).encode("utf-8"))
+            self.wfile.write(json.dumps(data).encode("utf8"))
+
         elif self.path == "/info":
             info = {"version": "1.0", "description":
                     "A simple API built with http.server"}
@@ -38,11 +40,13 @@ class HTTPserver(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(info).encode("utf8"))
+
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK")
+
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
