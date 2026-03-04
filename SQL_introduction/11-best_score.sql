00fc619365f439f09f5b117fca0d8def7ff95bfe
theocaulet <caulet.theo@gmail.com>
2026-03-04 09:28:50 +0100

    fix: Correct casing of SQL keywords in best_score query

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- List all records with a score >= 10
-select score, name from second_table where score >= 10
-order by score desc;
+SELECT score, name FROM second_table WHERE score >= 10
+ORDER BY score DESC;
