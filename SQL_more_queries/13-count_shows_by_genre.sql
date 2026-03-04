dab864f2aa8d0ddb0bccac2a591618fdbcfa8dd0
theocaulet <caulet.theo@gmail.com>
2026-03-04 11:05:25 +0100

    feat: Add SQL query to count shows by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- List all genres from hbtn_0d_tvshows.
+SELECT tv.genre AS genre, COUNT(*) AS number_of_shows
+FROM tv_show_genres
+JOIN genres AS tv ON tv_show_genres.genre_id = tv.id
+GROUP BY tv.genre
+ORDER BY number_of_shows DESC, tv.genre ASC;
