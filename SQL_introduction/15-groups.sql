bde36a9376512b6aae5d43d318a8afe156fb9216
theocaulet <caulet.theo@gmail.com>
2026-03-04 09:39:09 +0100

    fix: Add ORDER BY clause to group records by score in second_table

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
 -- List the number of records with the same score in the second_table.
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score
+ORDER BY score DESC;
