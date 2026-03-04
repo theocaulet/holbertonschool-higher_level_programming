a28543f420d4aeb1fbd5e6c50759bc8e5dc722de
theocaulet <caulet.theo@gmail.com>
2026-03-04 11:33:00 +0100

    fix: Remove genre_id from SQL query for comedy shows

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- List all comedy shows.
-SELECT tv_shows.title, tv_shows.genre_id
+SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
