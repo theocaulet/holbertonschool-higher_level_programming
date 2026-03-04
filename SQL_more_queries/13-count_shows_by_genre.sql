aa8847d59664ffab0e3a2ef0edbe1c2b709657d1
theocaulet <caulet.theo@gmail.com>
2026-03-04 11:06:38 +0100

    fix: Update GROUP BY clause to use alias for consistency

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv.genre AS genre, COUNT(*) AS number_of_shows
 FROM tv_show_genres
 JOIN genres AS tv ON tv_show_genres.genre_id = tv.id
-GROUP BY tv.genre
+GROUP BY genre
 ORDER BY number_of_shows DESC, tv.genre ASC;
