e1ce19165d08036dd69ea7b003012861baad2921
theocaulet <caulet.theo@gmail.com>
2026-03-04 09:42:22 +0100

    fix: Correct casing of SQL keywords in privileges query

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges of the MySQL user 'user_0d_1' and 'user_0d_2'.
-show grants for 'user_0d_1'@'localhost';
-show grants for 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
