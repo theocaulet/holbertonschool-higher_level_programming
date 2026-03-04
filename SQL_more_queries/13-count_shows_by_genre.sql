48bcefd8a2a52c943f2421794ed9ab2a2151fcaf
theocaulet <caulet.theo@gmail.com>
2026-03-04 11:09:37 +0100

    fix: Update GROUP BY clause to use full genre name for consistency

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_show_genres
 JOIN genres ON tv_show_genres.genre_id = genres.id
-GROUP BY genre
+GROUP BY genres.name
 ORDER BY number_of_shows DESC;
