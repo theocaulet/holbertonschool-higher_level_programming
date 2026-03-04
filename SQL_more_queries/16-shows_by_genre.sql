9d17cdc6b4e45f255db7183ff6c35ff7544d0902
theocaulet <caulet.theo@gmail.com>
2026-03-04 11:38:50 +0100

    fix: Correct SQL query to select genre name instead of genre_id

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- List all shows and their genres.
-SELECT tv_shows.title, tv_shows.genre_id
+SELECT tv_shows.title, tv_genre.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
