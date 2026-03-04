f7fb71389a6447b8a11d6e5c9a9bb9ac54fbc6fe
theocaulet <caulet.theo@gmail.com>
2026-03-04 11:14:13 +0100

    fix: Update SQL query to count shows by genre using specific show_id

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- List all genres from hbtn_0d_tvshows.
-SELECT genres.name AS genre, COUNT(*) AS number_of_shows
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
 JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
 GROUP BY genres.name
+HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
