fcf41ca09199888c8851e6a4a12d1dc7941ca864
theocaulet <caulet.theo@gmail.com>
2026-03-04 11:11:25 +0100

    fix: Correct SQL query to reorder JOIN clauses for clarity

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- List all genres from hbtn_0d_tvshows.
 SELECT genres.name AS genre, COUNT(*) AS number_of_shows
-FROM tv_show_genres
-JOIN genres ON tv_show_genres.genre_id = genres.id
+FROM genres
+JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
 GROUP BY genres.name
 ORDER BY number_of_shows DESC;
