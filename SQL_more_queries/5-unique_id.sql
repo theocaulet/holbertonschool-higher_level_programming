a19f9e0077ce8a7dbe7f0aacec354db26ba92bcb
theocaulet <caulet.theo@gmail.com>
2026-03-04 09:52:58 +0100

    fix: Correct casing of SQL keywords in unique_id table creation

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Create the table unique_id
-create table if not exists unique_id (
-    id int default 1 unique,
-    name varchar(256)
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
 )
