f0d3741636d0b44fd3e9eae36a74a1be3c29476c
theocaulet <caulet.theo@gmail.com>
2026-03-05 12:54:01 +0100

    feat: Add script to list all cities from the database hbtn_0e_4_usa

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Lists all cities from the database hbtn_0e_4_usa."""
+import MySQLdb
+import sys
+
+
+def main():
+    """Define the main function"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name" \
+                   " FROM cities" \
+                   " JOIN states ON cities.state_id = states.id" \
+                   " ORDER BY cities.id ASC")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
