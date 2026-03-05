75e8254c161ff863739e9777ef143bf6af07add5
theocaulet <caulet.theo@gmail.com>
2026-03-05 15:13:16 +0100

    feat: Add script to print a State object from the database

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Print a State object from the database hbtn_0e_6_usa."""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+def main():
+    """Define the main function"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+    engine = create_engine('mysql+mysqldb://{}:{}'
+                           '@localhost:3306/{}'.format(username, password,
+                                                       database))
+    session = sessionmaker(bind=engine)
+    session = session()
+    states = session.query(State).filter(State.name == search).first()
+    if states:
+        print("{}: {}".format(states.id, states.name))
+    else:
+        print("Not found")
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
