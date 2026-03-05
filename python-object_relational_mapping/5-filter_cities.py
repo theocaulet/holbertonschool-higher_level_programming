102bcf826137b825b374094a7e2345a43b78c485
theocaulet <caulet.theo@gmail.com>
2026-03-05 13:19:27 +0100

    feat: Add script to list all cities of a given state from the database hbtn_0e_4_usa

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that takes in the name of a state as an argument
+ and lists all cities of that state, using the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+
+def main():
+    """Define the main function"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name FROM cities"
+                   " JOIN states ON cities.state_id = states.id"
+                   " WHERE states.name = %s"
+                   " ORDER BY cities.id ASC", (state,))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
