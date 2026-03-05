75814e0a41cc93fe36e4f1d8231c85e2980b19f2
theocaulet <caulet.theo@gmail.com>
2026-03-05 11:26:46 +0100

    feat: Add script to list all states with names starting with 'N'

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with N."""
+import MySQLdb
+import sys
+
+
+def main():
+    """Define the main function"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name"
+                   " LIKE BINARY 'N% \'ORDER BY id ASC")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
