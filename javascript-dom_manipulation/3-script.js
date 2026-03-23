151a4dd8958725c5de93d53e52a11053c561718e
theocaulet <caulet.theo@gmail.com>
2026-03-23 11:17:00 +0100

    feat: Add scripts for DOM manipulation tasks including header color change, item addition, and character fetching

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,10 @@
+document.querySelector('#toggle_header').addEventListener('click', function () {
+  if (document.querySelector('header').classList.contains('red')) {
+    document.querySelector('header').classList.remove('red')
+    document.querySelector('header').classList.add('green')
+  }
+  else {
+    document.querySelector('header').classList.remove('green')
+    document.querySelector('header').classList.add('red')
+  }
+})
---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,4 @@
+document.querySelector('#update_header').addEventListener('click', function () {
+  const newheader = document.querySelector('header')
+  newheader.textContent = 'New Header!!!'
+})
