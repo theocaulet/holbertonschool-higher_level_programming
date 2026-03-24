e50ac749d9e5f8e1f67e10dcfbc9ff0b09a9fa7a
theocaulet <caulet.theo@gmail.com>
2026-03-24 10:10:12 +0100

    fix: Correct syntax by adding spaces in promise callback functions

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,7 +1,7 @@
 fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
-  .then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(data) {
+  .then(function (data) {
     document.querySelector('#character').textContent = data.name;
-  })
+  });
