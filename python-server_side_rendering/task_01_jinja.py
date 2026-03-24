29ca7b6688999974a1500ffdc3ac6cc54a2df24f
theocaulet <caulet.theo@gmail.com>
2026-03-24 14:48:35 +0100

    feat: Add Flask application with home, about, and contact pages using templates

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Render the home page."""
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    """Render the about page."""
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    """Render the contact page."""
+    return render_template('contact.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
