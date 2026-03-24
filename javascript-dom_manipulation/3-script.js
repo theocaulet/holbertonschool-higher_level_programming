b74aad1c5f0e8c56234a210c0400e7fc21893325
theocaulet <caulet.theo@gmail.com>
2026-03-24 10:07:23 +0100

    fix: Correct syntax by adding missing semicolons in header toggle function

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,10 +1,9 @@
 document.querySelector('#toggle_header').addEventListener('click', function () {
   if (document.querySelector('header').classList.contains('red')) {
-    document.querySelector('header').classList.remove('red')
-    document.querySelector('header').classList.add('green')
+    document.querySelector('header').classList.remove('red');
+    document.querySelector('header').classList.add('green');
+  } else {
+    document.querySelector('header').classList.remove('green');
+    document.querySelector('header').classList.add('red');
   }
-  else {
-    document.querySelector('header').classList.remove('green')
-    document.querySelector('header').classList.add('red')
-  }
-})
+});
