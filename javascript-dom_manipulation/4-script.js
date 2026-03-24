ca471c18bdee32f6a582f9af2a1bfd706fc6f0f6
theocaulet <caulet.theo@gmail.com>
2026-03-24 10:08:29 +0100

    fix: Correct syntax by adding missing semicolons in item addition function

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,5 +1,5 @@
 document.querySelector('#add_item').addEventListener('click', function () {
-  const newLi = document.createElement('li')
-  newLi.textContent = 'Item'
+  const newLi = document.createElement('li');
+  newLi.textContent = 'Item';
   document.querySelector('.my_list').appendChild(newLi);
-})
+});
